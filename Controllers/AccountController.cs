b14856f7bb6afdd454507d75fe7aa4b5ffda2a03
Muhammad Ahmad <FA19-BCS-010@cuiatd.edu.pk>
2022-04-24 15:28:10 +0500

    before partial login

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Appointment.Models;
+
+namespace MVC_Appointment.Controllers
+{
+    public class AccountController : Controller
+    {
+        public readonly ApplicationDBContext _dbContext;
+        public AccountController(ApplicationDBContext db)
+        {
+            _dbContext = db;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Utility/Helper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MVC_Appointment.Utility
+{
+    public static class Helper
+    {
+        public static string admin = "Admin";
+        public static string patient = "Patient";
+        public static string doctor = "Doctor";
+        public static List<SelectListItem> GetRolesDropDownList()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem{Value=Helper.admin, Text=admin},
+                new SelectListItem{Value=Helper.patient, Text=patient},
+                new SelectListItem{Value=Helper.doctor, Text=doctor}
+            };
+        }
+    }
+}
